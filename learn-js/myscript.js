8378c42cebed29c4bb83dad38a6a693478354081
swtysna <swtysna@gmail.com>
2024-07-16 10:46:58 +0530

    git status

---
+++ learn-js/myscript.js
@@ -0,0 +1,27 @@
+//console.log("get");
+// console.log("I am sonali");
+// document.getElementById("address").innerHTML = "My first house";
+// document.getElementById("submit").innerHTML = "click to submit";
+
+// function clickme(){
+// // alert("you clicked button");
+// document.getElementById("submit").innerHTML = "hahaha you clicked!";
+// document.getElementById("mobile").innerHTML = "Mobile no.- 456789123";
+
+// }
+// // this function will tell me about  myself
+// function infoMyself(){
+// //alert("click here! show some love");
+// document.getElementById("myself").innerHTML = "Hello, I am SWEETY. I want to become a proffesional makeup artist.";
+
+// }
+
+
+
+// document.getElementById("submit").onclick = clickme;
+
+// document.getElementById("infomyself").onclick = infoMyself;
+
+
+
+
