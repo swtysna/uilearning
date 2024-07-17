7c6e365d8e982e62ea0138b7d62fc6c677fdca41
swtysna <swtysna@gmail.com>
2024-07-17 18:21:47 +0530

    changes sum

--- learn-js/myscript.js
+++ learn-js/myscript.js
@@ -1,26 +1,64 @@
-//console.log("get");
-// console.log("I am sonali");
-// document.getElementById("address").innerHTML = "My first house";
-// document.getElementById("submit").innerHTML = "click to submit";
+console.log("get");
+console.log("I am sonali");
+document.getElementById("address").innerHTML = "My first house";
+document.getElementById("submit").innerHTML = "click to submit";
 
-// function clickme(){
-// // alert("you clicked button");
-// document.getElementById("submit").innerHTML = "hahaha you clicked!";
-// document.getElementById("mobile").innerHTML = "Mobile no.- 456789123";
+function clickme(){
+// alert("you clicked button");
+document.getElementById("submit").innerHTML = "hahaha you clicked!";
+document.getElementById("mobile").innerHTML = "Mobile no.- 456789123";
 
-// }
-// // this function will tell me about  myself
-// function infoMyself(){
-// //alert("click here! show some love");
-// document.getElementById("myself").innerHTML = "Hello, I am SWEETY. I want to become a proffesional makeup artist.";
+}
+// this function will tell me about  myself
+function infoMyself(){
+//alert("click here! show some love");
+document.getElementById("myself").innerHTML = "Hello, I am SWEETY. I want to become a proffesional makeup artist.";
 
-// }
+}
 
 
 
-// document.getElementById("submit").onclick = clickme;
+document.getElementById("submit").onclick = clickme;
 
-// document.getElementById("infomyself").onclick = infoMyself;
+document.getElementById("infomyself").onclick = infoMyself;
+
+/*
+1. create two input type text field in HTML code to enter nos.
+2. store input two nos. into varriables, say a, b 
+3. sum a & b and store into c. c = a + b
+4. display c 
+
+
+
+*/
+
+function sum() {
+    // get first no. and store into variable a
+    var a = document.getElementById("first-no").value;
+
+    var b = document.getElementById("second-no").value;
+
+
+
+    console.log(a);
+    console.log(b);
+// parseint use for coverting sring value to integer
+    var c = parseInt(a) + parseInt(b);
+
+    console.log(c);
+
+
+    document.getElementById("total").innerHTML = c;
+
+}
+
+document.getElementById("sum").onclick = sum;
+
+
+// TODO sum subtraction multiply divide
+// check odd even input no.
+
+
 
 
 
