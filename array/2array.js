e476960bf355e3f781a27ef4e8d7cc2f7271caea
swtysna <swtysna@gmail.com>
2024-09-05 19:36:43 +0530

    -- add two elements of two another arrays

--- array/2array.js
+++ array/2array.js
@@ -1,59 +1,56 @@
-// Take a array of  12 random numbers / integers
+// // Take a array of  12 random numbers / integers
 
-let arr = [12, 18, 67, 89, 90, 45, 56, 78, 38, 79, 55, 62]; // arr[0] // length = 12
+// let arr = [12, 18, 67, 89, 90, 45, 56, 78, 38, 79, 55, 62]; // arr[0] // length = 12
 
-// print 11th element of this array
+// // print 11th element of this array
 
-let eleventhElem = arr[10];
-console.log("== 11th element: " + eleventhElem);
+// let eleventhElem = arr[10];
+// console.log("== 11th element: " + eleventhElem);
 
-let firstElem = arr[0];
-console.log("== 1st element: " + firstElem);
+// let firstElem = arr[0];
+// console.log("== 1st element: " + firstElem);
 
-let fourthElem = arr[3];
-console.log("== 4th element: " + fourthElem);
+// let fourthElem = arr[3];
+// console.log("== 4th element: " + fourthElem);
 
-let lastIndex = arr.length - 1;// 11
-console.log("last Index of arr " + lastIndex); // 11
+// let lastIndex = arr.length - 1;// 11
+// console.log("last Index of arr " + lastIndex); // 11
 
-let lastElem = arr[lastIndex]; // arr[11] // 62
-console.log("== last element " + lastElem);
+// let lastElem = arr[lastIndex]; // arr[11] // 62
+// console.log("== last element " + lastElem);
 
-// check if this array fourth element is odd or even
+// // check if this array fourth element is odd or even
 
-function checkEvenAndOddArray(intArr) {
-    console.log("input number " + intArr );
+// function checkEvenAndOddArray(intArr) {
+//     console.log("input number " + intArr );
 
-    let firstElem = intArr[0];// first element will be in 0 index
+//     let firstElem = intArr[0];// first element will be in 0 index
   
-    let fourthElem = intArr[3];// 
+//     let fourthElem = intArr[3];// 
 
-  let lastIndex = intArr.length - 1; // to find last index no.
+//   let lastIndex = intArr.length - 1; // to find last index no.
   
-  let lastEle = intArr[lastIndex];
+//   let lastEle = intArr[lastIndex];
+//   console.log("== first element " + firstElem);
+  
+//   console.log("== last element " + lastEle);
+  
 
-    if(fourthElem % 2 === 0) {
-        console.log("fourth Element " + fourthElem + " is even");
-    }
-    else {
-        console.log("fourth Element " + fourthElem + " is odd");
-    }
-    if(lastElem % 2 !== 0) {
-        console.log("last Element " + lastElem + " is odd");  
-    }
-    else {
-        console.log("last Element " + lastElem + " is even");
-    }
-}
-
-checkEvenAndOddArray(44, 55, 99, 85, 75, 23);
+//     if(fourthElem % 2 === 0) {
+//         console.log("fourth Element " + fourthElem + " is even");
+//     }
+//     else {
+//         console.log("fourth Element " + fourthElem + " is odd");
+//     }
+//     if(lastElem % 2 !== 0) {
+//         console.log("last Element " + lastElem + " is odd");  
+//     }
+//     else {
+//         console.log("last Element " + lastElem + " is even");
+//     }
 
 
-// print mid number
+// }
 
-/* 12 /2 = 6
-Math.floor(6) = 6
+// checkEvenAndOddArray(arr);
 
-*/
-
-// let midIndex = math.floor()
---
+++ array/midnum.js
@@ -0,0 +1,15 @@
+// check mid num
+
+let arr2 = [15, 2, 43, 56, 78,24,12,90,79,67,57,88]
+
+
+  let midIndex = arr2.length / 2; // 12 / 2 = 6
+  console.log(midIndex);
+  let midIndex2 = arr2.length / 2 - 1; // 12 / 2 -1 = 5
+  
+  let midNum1 = arr2[midIndex];
+  console.log("== midNum " + midNum1);
+
+  let midNum2 = arr2[midIndex - 1];
+
+   console.log("== midNum " + midNum2);
