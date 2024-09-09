04f3f872f2b4124e065a66e77ebdff15a92b3fed
swtysna <swtysna@gmail.com>
2024-09-09 15:46:37 +0530

    -- added algo

--- array/addlastindex.js
+++ array/addlastindex.js
@@ -94,23 +94,13 @@ step 6. end
 
 // arrpush(arr3,"snake");
 
-//  converts an array to a string of (comma separated) array values.
-/*
-step:1 start
-step:2 create a function
-step:3 array will consist string
-step:4 covert array to a string of (comma separated)
-step:6 take variable 
-step:7 print it
-step:8 end
-*/
 
 
 // function arrpush(arr1,num) {
 //         console.log("input array " + arr1);
 //         console.log("input array " + num);
 
-
+// Q 1.
 //  arr1.push(num);
 //  console.log(arr1);
 
@@ -123,8 +113,10 @@ step:8 end
 // arrpush(arr1,"cat");
 
 
-/*
-step:1 start
+
+
+// Q  2.converts an array to a string of (comma separated) array values.
+/*step:1 start
 step:2 create a function
 step:3 array will consist string
 step:4 covert array to a string of (comma separated)
@@ -132,6 +124,7 @@ step:6 take variable
 step:7 print it
 step:8 end
 */
+
 
 // function arrtostr(str) {
 //     console.log("input array " + str);
@@ -147,7 +140,15 @@ step:8 end
 
 // arrtostr(r);
 
-
+/*Q 3. insert a new array into an array. array.pop
+step 1. start
+step 2. create a function with one arguements
+step 3. took a variable put elements
+step 4. pop the array
+step 5. print the array
+step 7. call function and watch the result
+step 8. end
+*/
 
 // function arrpop(arr) {
 //     console.log("input array " + arr);
@@ -164,6 +165,18 @@ step:8 end
 
 
 // arrpop(arr);
+
+
+/*
+Q 4.shift an element from an array. use arr.shift
+step 1. start
+step 2. create a function with one arguements
+step 3. use function name with shift like arr.shift
+step 4. shift the array
+step 5. print the array
+step 7. call function and watch the result array will shift from the left side
+step 8. end
+*/
 
 
 
@@ -183,18 +196,97 @@ step:8 end
 
 // arrshift(arr);
 
-function arrunshift(arr,a) {
-    console.log("input array " + arr);
-    console.log("input array " + a);
+/*
+Q 5.unshift an element from an array. use arr.unshift
+step 1. start
+step 2. create a function with one arguements
+step 3. use function name with unshift like arr.shift
+step 4. unshift the array
+step 5. print the array
+step 7. call function and watch the result array will unshift (add) from the left side
+step 8. end
+*/
+
+// function arrunshift(arr,a) {
+//     console.log("input array " + arr);
+//     console.log("input array " + a);
 
 
-arr.unshift(a);
-console.log(arr);
+// arr.unshift(a);
+// console.log(arr);
 
     
+// }
+// let arr = ["salad", "fruit", "juice", "nuts"];
+
+// arrunshift(arr,"lemon");
+
+
+/* 
+Q 6. concat two arrays
+step 1. start
+step 2. create a function which will accept two arguements both will be array
+step 3. arrays will consists number or string  , 
+step 4. took one variable to concact two rays // rrr = arr.concat(brr) // arr is first array // brr is second array
+step 5. print the array // rrr
+step 6. call function and check the result two array will add from the right side
+step 7: end
+*/
+
+
+// function arrconcat(arr,brr) {
+//     console.log("input array " + arr);
+//     console.log("input array " + brr);
+
+
+// let rrr = arr.concat(brr);
+// console.log(rrr);
+
+    
+// }
+// let arr = ["salad", "fruit", "juice", "nuts"];
+
+// let brr = ["burger", "pizza"]
+
+// arrconcat(arr,brr);
+
+/*
+Q 7.copy array
+step 1. start
+step 2. create a function which will accept one arguements
+step 3. arrays will consists number or string  , print it
+step 4. use arr.copywithin(2,0)
+step 5. print it
+step 6. call function and check which element have  index no. 1 it will be deleted from the array. leaves undefined holes in the array
+step 7: end
+*/
+
+
+// function arrcopy(arr) {
+//     console.log("input array " + arr);
+    
+
+
+//  arr.copyWithin(2,0);
+// console.log(arr);
+
+    
+// }
+// let arr = ["salad", "fruit", "juice", "nuts"];
+
+
+
+// arrcopy(arr);
+
+// loop
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+let fLen = fruits.length;
+
+let text = "<ul>";
+for (let i = 0; i < fLen; i++) {
+  text += "<li>" + fruits[i] + "</li>";
 }
-let arr = ["salad", "fruit", "juice", "nuts"];
+text += "</ul>";
 
 
-
-arrunshift(arr,"lemon");
