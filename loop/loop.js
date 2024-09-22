81631e4ecaa6a849c89c97be912ab85cab277d4c
swtysna <swtysna@gmail.com>
2024-09-22 16:41:50 +0530

    -- check even and odd numbers in an array -- check divisibility by 3 and 5 between ... numbers

--- loop/loop.js
+++ loop/loop.js
@@ -1,199 +1,191 @@
 
-/**
- * @author: Sonali
- * @description: function to test array of size 3 find largest no. in an input array. This function will accept one argument as an Array 
- * and will return updated input array.
- * @param {Array} arr
- * @returns updated input array arr
- */
-/*
-Algorithm
-Step 1: As our task is to do a coloumn array convert into an row array with seperated comma .
-So we will  in this function we will pass a parameter array.
-
-Step 2: So we have already created the function function name is repeat and input array is arr.
- 
-
-Step 3: after the above condition sets a variable before the loop starts (let i = 0).
-
-step 4 : defines the condition for the loop to run arr length.
-
-step 5: increases a value (i++) each time the code block in the loop has been executed.
-
-step 6: print the array.
-
-step 7: return the input array.
-
-step 8: took a variale put an array into it.
-
-step 9: call the function
-
-
-// */
-// function repeat(arr) {
-
-//     for (let i = 0; i < arr.length; i++) {
-//         console.log("The for loop is     " + arr[i]);
-
-//         }
-
-
-//       return arr;
-// }
-
-// let arr1 = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
-// let arr2 = ["sweety", "sonu", "sargun", "guddan",];
-//    repeat(arr1);
-//    repeat(arr2);
-
-
-
-// //   2. loop for
-//    function lFor(num) {
-
-//     for (let i = 0; i < 5; i++) {
-//         console.log("The for loop is     " +[i]);
-
-//         }
-
-
-//       return lFor;
-// }
-// let num = ""
-// lFor(num);
-
 // /**
 //  * @author: Sonali
-//  * @description: function to calculate sum of array numbers in an input array. This function will accept one arguement as an Array
-//  * and will return updated input array
-//  * 
-//  * @param {Array} arr 
+//  * @description: function to test array of size 3 find largest no. in an input array. This function will accept one argument as an Array 
+//  * and will return updated input array.
+//  * @param {Array} arr
 //  * @returns updated input array arr
 //  */
-// //1. write a function to calculate sum of array numbers in an array
+// /*
+// Algorithm
+// Step 1: As our task is to do a coloumn array convert into an row array with seperated comma .
+// So we will  in this function we will pass a parameter array.
 
-function sumarr(arr) {
-    // print which function calling, so that in console, you may identify this:
-    console.log("sumarr function calling....");
-    // declare a variable sum with the value as per the below
-    let sum = 0;
-    // for loop declare a variable i the value as per the below comparing the index value 0 to array length
-    // let arr = [23, 34, 77, 99, 324];
-    for (let i = 0; i < arr.length; i++) {
+// Step 2: So we have already created the function function name is repeat and input array is arr.
+ 
 
-        // i = 0 ; 0 < 5
-        sum = sum + arr[i]; // sum = sum + arr[i]
-        // i = 0, sum = 0 + arr[0] = 0 + 23 = 23
-        //i = 1, sum = 23 + arr[1] = 23 + 34 = 57
-        //i = 2, sum = 57 + arr[2] = 57 + 77 = 134
-        // i= 3, sum = 134 + arr[3] = 134 + 99 = 233
-        // i = 4, sum = 233 + arr[4] = 233 + 324 = 557
-        // i = 5, 5 < 5 false
-    }
-    // print the updated sum
-    console.log(sum);
-    //// return updated Array from this function
-    return sum;
+// Step 3: after the above condition sets a variable before the loop starts (let i = 0).
 
-}
+// step 4 : defines the condition for the loop to run arr length.
 
-// declare a variable arr of type array as per the below
-let arr = [23, 34, 77, 99, 324];
-//
-// arr function call with arguments of arr, save this function return value into a variable
-// sumarr(arr);
-let sum = sumarr(arr);
-// 
-console.log("sum of input array of arr is " + sum);
+// step 5: increases a value (i++) each time the code block in the loop has been executed.
+
+// step 6: print the array.
+
+// step 7: return the input array.
+
+// step 8: took a variale put an array into it.
+
+// step 9: call the function
 
 
+// // */
+// // function repeat(arr) {
 
-// // 2. search a number in an array.
+// //     for (let i = 0; i < arr.length; i++) {
+// //         console.log("The for loop is     " + arr[i]);
 
-
-function findaNum(arr, key) {
-    // Iterate through the array
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === key) {
-            console.log("Search found");
-            return true; // Return true when the key is found
-        }
-    }
-    console.log("Search not found");
-    return false; // Return false if the key is not found
-}
-
-let arr1 = [53, 76, 78, 34, 56];
-
-findaNum(arr1, 99); // Output: Search not found
-findaNum(arr1, 78); // Output: Search found
-findaNum(arr1, 53); // output: search found
+// //         }
 
 
-// let number = 25;
+// //       return arr;
+// // }
+
+// // let arr1 = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+// // let arr2 = ["sweety", "sonu", "sargun", "guddan",];
+// //    repeat(arr1);
+// //    repeat(arr2);
+
+
+
+// // //   2. loop for
+// //    function lFor(num) {
+
+// //     for (let i = 0; i < 5; i++) {
+// //         console.log("The for loop is     " +[i]);
+
+// //         }
+
+
+// //       return lFor;
+// // }
+// // let num = ""
+// // lFor(num);
+
+// // /**
+// //  * @author: Sonali
+// //  * @description: function to calculate sum of array numbers in an input array. This function will accept one arguement as an Array
+// //  * and will return updated input array
+// //  * 
+// //  * @param {Array} arr 
+// //  * @returns updated input array arr
+// //  */
+// // //1. write a function to calculate sum of array numbers in an array
+
+// function sumarr(arr) {
+//     // print which function calling, so that in console, you may identify this:
+//     console.log("sumarr function calling....");
+//     // declare a variable sum with the value as per the below
+//     let sum = 0;
+//     // for loop declare a variable i the value as per the below comparing the index value 0 to array length
+//     // let arr = [23, 34, 77, 99, 324];
+//     for (let i = 0; i < arr.length; i++) {
+
+//         // i = 0 ; 0 < 5
+//         sum = sum + arr[i]; // sum = sum + arr[i]
+//         // i = 0, sum = 0 + arr[0] = 0 + 23 = 23
+//         //i = 1, sum = 23 + arr[1] = 23 + 34 = 57
+//         //i = 2, sum = 57 + arr[2] = 57 + 77 = 134
+//         // i= 3, sum = 134 + arr[3] = 134 + 99 = 233
+//         // i = 4, sum = 233 + arr[4] = 233 + 324 = 557
+//         // i = 5, 5 < 5 false
+//     }
+//     // print the updated sum
+//     console.log(sum);
+//     //// return updated Array from this function
+//     return sum;
+
+// }
+
+// // declare a variable arr of type array as per the below
+// let arr = [23, 34, 77, 99, 324];
+// //
+// // arr function call with arguments of arr, save this function return value into a variable
+// // sumarr(arr);
+// let sum = sumarr(arr);
+// // 
+// console.log("sum of input array of arr is " + sum);
+
+
+
+// // // 2. search a number in an array.
+
+
+// function findaNum(arr, key) {
+//     // Iterate through the array
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] === key) {
+//             console.log("Search found");
+//             return true; // Return true when the key is found
+//         }
+//     }
+//     console.log("Search not found");
+//     return false; // Return false if the key is not found
+// }
+
+// let arr1 = [53, 76, 78, 34, 56];
+
+// findaNum(arr1, 99); // Output: Search not found
+// findaNum(arr1, 78); // Output: Search found
+// findaNum(arr1, 53); // output: search found
+
+
+// // let number = 25;
+// // let squareRoot = Math.sqrt(number);
+// // console.log(squareRoot); // Output: 5
+
+
+// let number = 144
 // let squareRoot = Math.sqrt(number);
-// console.log(squareRoot); // Output: 5
-
-
-let number = 144
-let squareRoot = Math.sqrt(number);
-console.log(squareRoot); // output :12
-// 3. check input number is a prime number or not
-function isPrime(num) {
+// console.log(squareRoot); // output :12
+// // 3. check input number is a prime number or not
+// function isPrime(num) {
     
-    for (let i = 2; i <= Math.sqrt(num); i++) { // Check divisibility up to sqrt(num)
-        if (num % i === 0) {
-            console.log("This number is not a prime number " + num);
-            return true; // If no divisors, it's prime
-        }
+//     for (let i = 2; i <= Math.sqrt(num); i++) { // Check divisibility up to sqrt(num)
+//         if (num % i === 0) {
+//             console.log("This number is not a prime number " + num);
+//             return true; // If no divisors, it's prime
+//         }
        
-    }
+//     }
     
  
-        console.log("This number is a prime number " + num);
+//         console.log("This number is a prime number " + num);
     
-    return false; // If divisible, it's not prime
-}
+//     return false; // If divisible, it's not prime
+// }
 
    
   
-    isPrime(50);
-    isPrime(17);
+//     isPrime(50);
+//     isPrime(17);
 
-    function isPrime(num) {
-        if (num <= 1) return false; // Numbers less than or equal to 1 are not prime
-        for (let i = 2; i < num; i++) {
-            if (num % i === 0) {
-                console.log("This number is not a prime number " + num);
-                return false; // If num is divisible by any number other than 1 and itself
-            }
+//     function isPrime(num) {
+//         if (num <= 1) return false; // Numbers less than or equal to 1 are not prime
+//         for (let i = 2; i < num; i++) {
+//             if (num % i === 0) {
+//                 console.log("This number is not a prime number " + num);
+//                 return false; // If num is divisible by any number other than 1 and itself
+//             }
            
-        }
+//         }
         
-            console.log("This number is a prime number " + num);
-        return true; // If no divisors were found, num is a prime number
-    }
+//             console.log("This number is a prime number " + num);
+//         return true; // If no divisors were found, num is a prime number
+//     }
   
-    isPrime(41);
-    isPrime(85);
+//     isPrime(41);
+//     isPrime(85);
 
 // 4. check input string is palindrome or not?
-
 function reverse(str) {
-    console.log("input string = " + str);
-    let string = "sonali";
-    // variable holds reverse string
-    let revstr = " ";
-    for(let i = str.length - 1; i = i > 0; i--){
-        revstr += str[i];
-      
-    }
-      // return reverse string
-    
+    console.log("input string " +str);
+    let revStr = " ";
+
+    for(let i = 5; i > str.length - 1; i--);
+
+
 }
-
-console.log(revstr); // output : ilanos
-
 
 
 /**
@@ -207,29 +199,30 @@ console.log(revstr); // output : ilanos
  * i = 0; 4 - (0 + 1) = 4 - 1 = 3
  * i = 0; 3 - (0 + 1) = 3 - 1 = 2
  * i = 0; 2 - (0 + 1) = 2 - 1 = 1
- * i = 0; 1 - (0 + 1) = 1 - 1 = 1
+ * i = 0; 1 - (0 + 1) = 1 - 1 = 0
  * 
  * console.log(str)
+ * reversestr = reverse + str[i];
  * }
  * 
- */
+//  */
 
-function isPalindrome(str) {
-    reverse_str = reverse(str);
+// function isPalindrome(str) {
+//     reverse_str = reverse(str);
     
     
-    // same as string it is palindrome
-    // else not a palindrome
-    if (reverse_str === str) {
-        console.log("passed string is palindrome ");
-    }
-    else {
-        console.log("passed string is not palindrome");
-    }
+//     // same as string it is palindrome
+//     // else not a palindrome
+//     if (reverse_str === str) {
+//         console.log("passed string is palindrome ");
+//     }
+//     else {
+//         console.log("passed string is not palindrome");
+//     }
 
-}
+// }
 
-// isPalindrome("radar");
+// // isPalindrome("radar");
 
-// isPalindrome("pop"); 
+// // isPalindrome("pop"); 
 
---
+++ loop/utils.js
@@ -0,0 +1,194 @@
+
+/**
+ * utility function related to loop
+ * 
+ */
+
+
+/**
+ *1. write a function to print even no. between 1 to 100.
+ * start range = 1 ; End range = 100;
+ */
+function printEvenNums() {
+    // i = 1; 1 <= 100; true
+    // i = 2; 2 <= 100; true
+    // i = 3; 3 <= 100; true; for loop ke under jaayega.
+    // i = .......... 98 ; same 
+    // i = 99; 99 <= 100; true 
+    // i = 100; 100 <= 100; true
+    // i = 101; 101 <= 100; false
+    for (let i = 1; i <= 100; i++) {
+        // 1 % 2 === 0; false
+        // 2 % 2 === 0; true
+        // 3 % 2 === 0; false
+        // 99 % 2 === 0; false
+        // 100 % 2 === 0; true
+        if (i % 2 === 0) {
+            console.log("Even number is " + i);
+        }
+        // i++ ; i = 1 + 1 = 2;
+        // i++; i = 2 + 1 = 3;
+        // i++...... 98
+        // i++; i = 99; i = 99 + 1 = 100;
+        // i++; i = 100; i = 100 + 1= 101;
+    }
+}
+// print even number
+// calling this function..
+// printEvenNums();
+
+
+/**
+ *1. write a function to print even no. between 300 to 500.
+ * start range = 300 ; End range = 500;
+ */
+
+function printEvenNums1() {
+    for (let i = 300; i <= 500; i++) {
+        if (i % 2 === 0) {
+            console.log("Even number is " + i);
+        }
+    }
+}
+//   printEvenNums1();
+
+
+/**
+ *1. write a function to print even no. between 1000 to 2000.
+ * start range = 1000 ; End range = 2000;
+ */
+
+function printEvenNums2() {
+    for (let i = 1000; i <= 2000; i++) {
+        if (i % 2 === 0) {
+            console.log("Even number is " + i);
+        }
+    }
+}
+//   printEvenNums2();
+
+
+/**
+ *1. write a function to print even no. between two numbers.
+ * start range = num1; End range = num2;
+ * 
+ */
+
+function printEvenNums2(num1, num2) {
+    console.log("===================== input range ", num1, num2);
+   
+    for (let i = num1; i <= num2; i++) {
+        if (i % 2 === 0) {
+
+
+            console.log("Even number is " + i);
+           
+        }
+    }
+    
+}
+//calling the function..
+//   printEvenNums2(5,50);
+//   printEvenNums2(25,175);
+//   printEvenNums2(550,650);
+
+// let evenNum =  printEvenNums2(550,650);
+// console.log(" all even nos. ", evenNum); 
+
+/**
+ * 2. Write a function to print all odd numbers in an array between 1 to 100. Ex: 1, 3, 5, 7, ....... , 97, 99, 
+ */
+
+function printOddNums() {
+
+    let arr = [];
+    console.log("===================== input range ", arr);
+    for (let i = 1; i <= 100; i++) {
+        if (i % 2 !== 0) {
+            arr.push(i);
+        }
+
+
+    }
+    console.log("All Odd number is ", arr);
+    return arr; // this will contain all odd nos. between 1 - 100;
+
+
+}
+//calling the function...
+// printOddNums();
+
+/**
+ * write a function to return all even numbers between two numbers.
+ * strat range = arr1 ; End range = arr2;
+ * start create a blank variable to store all even nos.
+ * let arr = [];
+ * match even no. put in this array, put all match even no. in this array.
+ * return arr; // this will contain all even nos.
+ * 
+ */
+
+
+function printEvenArr(arr1, arr2) {
+    console.log("===================== input range ", arr1, arr2);
+    let arr = []; 
+    for (let i = arr1; i <= arr2; i++) {
+        if (i % 2 === 0) {
+            arr.push(i);
+        }
+    }
+    console.log(" all even nos. is ", arr);
+    return arr; // this will contain all even nos.
+}
+//calling the function..
+// let evenNum = printEvenArr(899, 1000);
+/**
+ * 1. Write a function to print all even numbers between 1 to 100. Ex: 2, 4, 6, 8, ....... , 98, 100
+ 2. Write a function to print all odd numbers between 1 to 100. Ex: 1, 3, 5, 7, ....... , 97, 99
+ 3. Write a function to print all numbers divisible by 3 between 1 to 100. Ex: 3, 6, 9, ...... 96, 99
+ 4. Write a function to print all numbers divisible by 5 between 100 to 200.
+ */
+ //3. Write a function to print all numbers divisible by 3 between 1 to 100. Ex: 3, 6, 9, ...... 96, 99
+ /**
+  * create a function
+  * start range = 1; end range = 100;
+  * for loop
+  * check numbers are divisible by 3
+  * print it
+  * call the function
+  */
+
+ function printDivNum(){
+    for(let i = 1; i <= 100; i++){
+    if(i % 3 === 0){
+        console.log(" This no. is divisible by 3 " +i);
+
+    }
+
+ }
+}
+// calling the function..
+// printDivNum();
+
+// 4. Write a function to print all numbers divisible by 5 between 100 to 200.
+/**
+ * 
+  * create a function
+  * start range = 100; end range = 200;
+  * for loop
+  * check numbers are divisible by 
+  * print it
+  * call the function
+  */
+  
+ function printDivNums(){
+    for(let i = 100; i <= 200; i++){
+    if(i % 5 === 0){
+        console.log(" This no. is divisible by 5 " +i);
+
+    }
+
+ }
+}
+// calling the function..
+printDivNums();
